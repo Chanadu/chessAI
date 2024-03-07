f80dbc78f94eddbfabe0e02f7c58f44621f4c515
Chanadu <chandu.peddada@gmail.com>
2024-03-06 22:21:39 -0600

    qwe

--- src/main.go
+++ src/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	chess "github.com/Chanadu/chessAI/chess"
+	chess "github.com/Chanadu/chessAI/src/chess"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-const ScreenWidth = 800
-const ScreenHeight = 800
-const SquareSize = min(ScreenWidth, ScreenHeight) / 8
-
-var LightBoardColor = rl.Color{234, 233, 210, 255}
-var DarkBoardColor = rl.Color{75, 115, 153, 255}
-
 func main() {
+	var ScreenWidth int32 = 800
+	var ScreenHeight int32 = 800
+	var SquareSize float64 = float64(min(ScreenWidth, ScreenHeight) / 8.0)
+
+	var LightBoardColor = rl.Color{234, 233, 210, 255}
+	var DarkBoardColor = rl.Color{75, 115, 153, 255}
+
 	rl.InitWindow(ScreenWidth, ScreenHeight, "chessAI")
 	defer rl.CloseWindow()
 
